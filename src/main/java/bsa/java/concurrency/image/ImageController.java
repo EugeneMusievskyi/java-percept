849384c8d4ceca81b23ba63cc06cd0069570b461
Zhenya Musievsky <zhenya.musievsky@gmail.com>
2020-07-17 04:30:53 +0300

    request logging added

--- src/main/java/bsa/java/concurrency/image/ImageController.java
+++ src/main/java/bsa/java/concurrency/image/ImageController.java
@@ -1,6 +1,7 @@
 package bsa.java.concurrency.image;
 
 import bsa.java.concurrency.image.dto.SearchResultDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequestMapping("/image")
 public class ImageController {
@@ -19,24 +21,28 @@ public class ImageController {
     @PostMapping("/batch")
     @ResponseStatus(HttpStatus.CREATED)
     public void batchUploadImages(@RequestParam("images") MultipartFile[] files) {
+        log.info("image/batch request. files length: " + files.length);
         imageService.save(files);
     }
 
     @PostMapping("/search")
     @ResponseStatus(HttpStatus.OK)
     public List<SearchResultDTO> searchMatches(@RequestParam("image") MultipartFile file, @RequestParam(value = "threshold", defaultValue = "0.9") double threshold) {
+        log.info(String.format("image/search request. file name: %s, file size: %d, threshold: %d", file.getName(), file.getSize(), threshold));
         return imageService.searchImage(file, threshold);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteImage(@PathVariable("id") UUID imageId) {
+        log.info(String.format("image/%s request", imageId));
         imageService.deleteImage(imageId);
     }
 
     @DeleteMapping("/purge")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void purgeImages() {
+        log.info("image/purge request");
         imageService.purgeImages();
     }
 }
